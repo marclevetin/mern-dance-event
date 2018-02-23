95f87f59ae1462475746d1bece63a4cc50f0bee1
marclevetin <marc@derbyhat.org>
2018-02-23 01:36:53 -0500

    added judge schema

---
+++ models/judge.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const judgeSchema = new Schema({
+  firstName: { type: String, required: [true, 'First name is required'] },
+  lastName: { type: String, required: [true, 'Last name is required'] },
+});
+
+judgeSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' } this.lastName;
+})
+
+const Judge = mongoose.model("Person", judgeSchema);
+
+module.exports = Judge;
