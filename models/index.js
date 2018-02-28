25322df0572f2c7f0b7c3e3f98062a26841ad328
marclevetin <marc@derbyhat.org>
2018-02-28 16:56:01 -0500

    fixed pluralization convention for some model files.  Updated model index.js to include all models in the default module.export

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  Book: require("./book")
+  Book: require("./book"),
+  Person: require("./person"),
+  Judge: require("./judge"),
+  ContestDetail: require("./contestDetail"),
+  ContestResult: require("./contestResult"),
+  ContestScore: require("./contestScore"),
+  Event: require("./event")
 };
